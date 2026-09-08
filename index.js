fcbe3d040c114e04ec5e47992cf14e91ad68e3bf
Hrishikesh Bhoi <hrishikeshbhoi09@gmail.com>
2026-09-08 22:13:40 +0530

    Add BE1.2_HW1

---
+++ index.js
@@ -0,0 +1,34 @@
+const { initializeDatabase } = require("./db/db.connect.js");
+const fs = require("fs");
+const Profile = require("./models/profile.models.js")
+
+
+initializeDatabase();
+
+const jsonData = fs.readFileSync('profiles.json', 'utf-8');
+const profilesData = JSON.parse(jsonData);
+
+function seedData() {
+    try{
+        for(const profileData of profilesData) {
+            const newProfile = new Profile({
+                fullName: profileData.fullName,
+                username: profileData.username,
+                bio: profileData.bio,
+                profilePicUrl: profileData.profilePicUrl,
+                followingCount: profileData.followerCount,
+                followerCount: profileData.followerCount,
+                companyName: profileData.companyName,
+                location: profileData.location,
+                portfolioUrl: profileData.portfolioUrl,
+
+            });
+
+            newProfile.save();
+        }
+
+    } catch(error) {
+        console.log("Error seeding the data", "error");
+    }
+}
+seedData();
---
+++ models/profile.models.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+
+const profileSchema = new mongoose.Schema({
+    fullName: String,
+    username: String,
+    bio: String,
+    profilePicUrl: String,
+    followingCount: Number,
+    followerCount: Number,
+    companyName: String,
+    location: String,
+    portfolioUrl: String,
+});
+
+const Profile = mongoose.model("Profile", profileSchema);
+module.exports = Profile;
